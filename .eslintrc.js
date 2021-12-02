2cfdd0ac69c2b632d76abceb2ff8311f7cc246f8
Shirin Etezazian <64745415+setezazian@users.noreply.github.com>
2021-12-02 14:34:48 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,19 +4,15 @@ module.exports = {
     commonjs: true,
     es2021: true,
     node: true,
+    jest: true,
   },
   extends: [
-    'plugin:react/recommended',
     'airbnb',
   ],
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
     ecmaVersion: 13,
   },
   plugins: [
-    'react',
   ],
   rules: {
   },
